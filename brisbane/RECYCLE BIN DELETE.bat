d3293ddaa3fc78d935677c0b4f118f082824caaf
Active Directory Admin <teamadmin@THEPHOTOSTUDIO.local>
2020-10-09 16:49:53 +1000

    Arpan initial version of CREATE_PS_ALBUMS

---
+++ brisbane/RECYCLE BIN DELETE.bat
@@ -0,0 +1,1 @@
+del C:\$RECYCLE.BIN /s /f /q
---
+++ brisbane/mystratus_sessionstatus_bulk_query.ps1
@@ -0,0 +1,37 @@
+﻿Function Get-Filename($directory)
+{
+    [System.reflection.assembly]::LoadWithPartialName("System.windows.forms")|Out-null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $directory
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+    
+}
+
+$dir = Get-Filename "C:\archiveworks\"
+$file = Split-path $dir -leaf
+$sessions = Get-Content $dir
+echo $sessions.Length
+
+
+$api_url = "https://api.thephotostudio.com.au/sp/Session?action=sessionnumber&value="
+echo " " 
+
+$Headers = @{
+            "x-api-key"="1zpqpC9Gk57wCXVB46UKkv4sWFRzxc99jRz9RND8"            }
+
+ for($i=0;$i -lt $sessions.length -1;$i++)
+ {
+ echo $("Querying session: " + $sessions[$i])
+ try
+ {
+ $session_data = Invoke-RestMethod -Uri $($api_url + $sessions[$i].Substring(0,$sessions[$i].IndexOf('_'))) -Headers $Headers
+ echo $("   Session Status: " + $session_data.StatusDescription);
+ Add-Content $('C:\archiveworks\Session_Status_Report_'+ $file + ".csv") $($sessions[$i] + "," + $session_data.StatusDescription)
+ }
+ catch [System.Net.WebException]
+ {
+  echo $("   Session not found");
+ Add-Content $('C:\archiveworks\Session_Status_Report_'+ $file + ".csv") $($sessions[$i].Substring(0,$sessions[$i].IndexOf('_')) + "," + "Session Not Found")
+ }
+ }
